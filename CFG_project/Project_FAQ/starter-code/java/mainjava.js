f4b07cb7222ace5197f627e642a82c401a2a1d58
bellarosemarie <85574884+bellarosemarie@users.noreply.github.com>
2021-06-08 18:56:23 +0200

    Added files to project

---
+++ CFG_project/Project_FAQ/starter-code/java/mainjava.js
@@ -0,0 +1,50 @@
+//add alert box to inform customers on longer delivery times due to covid-19 as well as extended return rights from 14 days to 28 days after the order has been 
+
+
+
+function myFunction1() {
+    var x = document.getElementById("return");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+
+  function myFunction2() {
+    var x = document.getElementById("delays");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction3() {
+    var x = document.getElementById("costs");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction4() {
+    var x = document.getElementById("payment");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction5() {
+    var x = document.getElementById("shipping");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
